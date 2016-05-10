6d261c73baa46037aff932b54b595467f71f1052
Jared Mahan <jaredmahan@gmail.com>
2016-05-10 13:11:08 -0500

    Fixed markup for tabs.

--- public/app/core/config.js
+++ public/app/core/config.js
@@ -4,8 +4,8 @@
     var core = angular.module('app.core');
 
     var config = {
-        appTitle: 'Project Name',
-        version: '1.0.0.0',
+        appTitle: 'Angular Search Filter',
+        version: 'alpha-0-0.0.0.9',
     };
     core.value('config', config);
     
